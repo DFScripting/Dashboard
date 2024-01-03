6acf5a49037acfa343d6603e3b7ed475679ffc8b
TechStreet <dftechstreet@gmail.com>
2024-01-03 10:29:20 +0000

    add: script endpoint

---
+++ src/api/Script.ts
@@ -0,0 +1,25 @@
+import type { AstroGlobal } from "astro";
+import API from "./API";
+
+export const api = new API();
+
+export interface APIScriptResponse {
+    id: number;
+    name: string;
+    description: string;
+    version: number;
+    approved: boolean;
+    headers: {
+        
+    }
+}
+
+export default async function getApplication(Astro: AstroGlobal, id: string) {
+	let header = new Headers();
+	header.append('X-Token', Astro.cookies.get("access_token").value);
+	header.append('X-Script', id);
+
+	return await api.get<APIScriptResponse>(`dfscript/web/script/`, {
+		headers: header
+	});
+}
--- src/api/User.ts
+++ src/api/User.ts
@@ -3,74 +3,17 @@ import API, { APIErrorResponse } from "./API";
 
 export const api = new API();
 
-export async function getUserData(Astro: AstroGlobal) : Promise<APIUserResponse | APIErrorResponse> {
-	let userData: APIUserResponse | APIErrorResponse;
-	if (Astro.cookies.has("astronoSession")) {
-		const rawData = Astro.cookies.get("astronoSession").value;
-		userData = JSON.parse(String(rawData))
-	} else {
-		let rawData = Astro.cookies.get("discord").value;
-		if (rawData == undefined) rawData = '{"error": "not logged in", "id":"unknown"}'
-		const parsedData = JSON.parse(String(rawData))
-		userData = await getUser(parsedData.id)
-		if (API.didError(userData) && userData.error == "log out") {
-			Astro.redirect("/discord/logout/");
-			return userData;
-		}
-		
-		const moment = new Date();
-		moment.setSeconds(moment.getDate() + 2);
-		Astro.cookies.set("astronoSession", JSON.stringify(userData), {
-			expires: moment, secure: true, path: "/", sameSite: "lax"
-		});
-	}
-
-	return userData;
-}
-
 export interface APIUserResponse {
-	status: string;
-	token: string;
-
-	discordId: number;
-
-	avatar: string;
 	username: string;
 	uuid: string;
-
-	admin: number;
-	dev: number;
-	mod: number;
-	builder: number;
-	artist: number;
-	tester: number;
-	donor: number;
-	
-	pack: string;
-	servers: {
+	scripts: {
+		id: number;
 		name: string;
-		serverId: string;
-		type: string;
-		channel: string;
-
-		playerCount: number;
-		max_players: number;
-
-		ram: number;
-		max_ram: number;
-
-		online: number;
-		ip: number;
-		port: string;
-		restart: string;
-	}[];
-	
-	reports: {
-		reporter: string;
-		reported: string;
-		reason: string;
-		timeReported: number;
-	}[];
+		description: string;
+		version: number;
+		approved: boolean;
+		approvedBy: string;
+	}[]
 }
 
 export default async function getUser(Astro: AstroGlobal) {
